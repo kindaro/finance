3dc6a481af9e2a3a13d59928e36c3df42b6fbccc
Ignat Insarov <kindaro@gmail.com>
2021-03-28 18:19:37 +0500

    Add some haddocks.

--- Finance.hs
+++ Finance.hs
@@ -5,11 +5,14 @@ import Data.Function
 
 import TypedArithmetic
 
+-- | This is the constant _e_.
 e ∷ Dimensionless
 e = exp 1
 
+-- | Discrete interest compounding.
 discrete ∷ Rate → DiscreteTime → Money → Money
 discrete interest = fix \ f t x → if t ≤ 0 then x else f (t - 1) (x × (interest + 1) × (1 ∷ Time))
 
+-- | Continuous interest compounding.
 continuous ∷ Rate → Time → Money → Money
 continuous r t x = (e°(r × t)) × x
