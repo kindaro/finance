f9e8b97082c183fe064d338b84d0f2c045252cdc
Ignat Insarov <kindaro@gmail.com>
2021-05-08 19:17:36 +0500

    Fix type of interest rates.
    The type is not really a frequency. It is a quotient of money by money,
    so it is actually dimensionless.

--- Finance.hs
+++ Finance.hs
@@ -13,19 +13,19 @@ e ∷ Dimensionless
 e = exp 1
 
 -- | Discrete interest compounding.
-discrete ∷ Rate → DiscreteTime → Money → Money
-discrete interest = fix \ f t x → if t ≤ 0 then x else f (t - 1) (x × (interest + 1) × (1 ∷ Time))
+discrete ∷ Dimensionless → DiscreteTime → Money → Money
+discrete interest = fix \ f t x → if t ≤ 0 then x else f (t - 1) (x × (interest + 1))
 
 -- | Continuous interest compounding.
-continuous ∷ Rate → Time → Money → Money
-continuous r t x = (e°(r × t)) × x
+continuous ∷ Dimensionless → Time → Money → Money
+continuous r t x = (e°(r × t × samplingRate)) × x
 
 -- | Continuous interest compounding with an additional steady income.
 -- Δ$ = r$Δt + αΔt.
-continuousWithLinearIncome ∷ Rate → Time → (Money ÷ Time) → Money → Money
+continuousWithLinearIncome ∷ Dimensionless → Time → (Money ÷ Time) → Money → Money
 continuousWithLinearIncome r t α x = case r of
   0 → x  +  α × t
-  r' → e°(r' × t) × x  +  α ÷ r'
+  r' → e°(r' × t × samplingRate) × x  +  α ÷ (r' × samplingRate)
 
 samplingRate ∷ Rate
 samplingRate = 1
