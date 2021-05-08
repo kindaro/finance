ced5f886db7aabcdc238e505b4f9cbccc52ca8aa
Ignat Insarov <kindaro@gmail.com>
2021-05-08 15:05:00 +0500

    Work in progress.

--- Finance.hs
+++ Finance.hs
@@ -1,9 +1,12 @@
 module Finance where
 
-import Prelude.Unicode ((≤), (∧), (≡))
+import Prelude.Unicode ((≤), (∧), (≡), (∘))
 import Data.Function
+import Data.Map qualified as Map
 
 import TypedArithmetic
+
+type key ⇸ value = Map.Map key value
 
 -- | This is the constant _e_.
 e ∷ Dimensionless
@@ -32,3 +35,14 @@ discountedPriceToSpotRate maturity price = (reciprocal price × (1 ∷ Money ÷ 
 
 spotRateToDiscountedPrice ∷ Time → Rate → Money
 spotRateToDiscountedPrice maturity rate = reciprocal ((1  +  rate)°(maturity × samplingRate)) × (1 ∷ Money ÷ Time)
+
+spotRatesToForwardRates ∷ Time ⇸ Rate → Time ⇸ Rate
+spotRatesToForwardRates = snd ∘ Map.mapAccumWithKey f (0 ∷ Time, 0 ∷ Rate)
+  where
+    f ∷ (Time, Rate) → Time → Rate → ((Time, Rate), Rate)
+    f (previousTime, previousRate) time rate =
+      let forwardRate = ((rate  +  1)°(time × samplingRate) ÷ (previousRate  +  1)°(previousTime × samplingRate) × (1 ∷ Rate)) ° reciprocal ((time  -  previousTime) × samplingRate)  -  1
+      in ((time, rate), forwardRate)
+
+forwardRatesToSpotRates ∷ Time ⇸ Rate → Time → Money
+forwardRatesToSpotRates forwardRates = _u
